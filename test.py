3b6e54fc5414e8eee71e6e38bc2426d11bea6602
pUrGe12 <helloitsme0218@gmail.com>
2025-12-09 02:56:18 +0530

    updated gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-from pybinwalk import scan, extract
-
-path = "/home/purge/Downloads/Team-battery.jpg"
-output_path = "/home/purge/Downloads/extracted_output_121"
-a = scan(path)
-b = extract(path, output_path)
-print(a)
-print(b)
